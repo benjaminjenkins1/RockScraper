2117a3aaf339f7759243f75f1f7d988969aa95e4
Benjamin <benjamin.jenkins1@marist.edu>
2017-02-07 17:03:41 -0500

    typo

--- RockScraper.py
+++ RockScraper.py
@@ -4,8 +4,6 @@ import requests
 page = requests.get('http://www.rockefeller100.org/items/show/4638')
 tree = html.fromstring(page.content)
 
-print(tree)
-
 tags = tree.xpath('//div[@id="item-metadata"]/dl/dd/a[@reg="tag"]/text()')
 
 print('tags: ',tags)
