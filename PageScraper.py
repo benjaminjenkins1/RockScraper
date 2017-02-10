344674616adeba9dc3217a60f450844a0c81a72d
Benjamin <benjamin.jenkins1@marist.edu>
2017-02-10 15:28:06 -0500

    scraper for generic pages

--- PageScraper.py
+++ PageScraper.py
@@ -1,1 +1,45 @@
+from lxml import html
+import requests
+import urllib.request
+import httplib2
+import string
+import json
+import os
 
+#Scraper for pages on Rockefeller100.org
+
+def getChildren(parent):
+  print('<'+parent.tag+'>')
+  print(parent.text)
+  try:
+    children = parent.getchildren()
+    for child in children:
+      getChildren(child)
+  except:
+    pass
+  print('</'+parent.tag+'>')
+
+
+path = input('path: ')
+url = input('url: ')
+
+h = httplib2.Http()
+
+while(not url == ''):
+  resp = h.request(url, 'HEAD')
+  if int(resp[0]['status']) == 200 :
+    
+    page = requests.get(url)
+    tree = html.fromstring(page.content)
+    
+    content = tree.xpath('//div[@class="content"]')
+    #content = tree.xpath('/*')
+    for item in content:
+      getChildren(item)
+    
+    #images)
+    
+    
+    url = input('url: ')
+  else:
+    print('server did not respond with status 200')
